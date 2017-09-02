02924a24a7a5d93083b3c2e88c3400dc2a81790d
Student <coolhighjumper@gmail.com>
2017-09-02 16:24:12 +0800

    class practice

---
+++ src/crawler/example/Practice/FacebookExample.java
@@ -0,0 +1,64 @@
+package crawler.example.Practice;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+/**
+ * 資料探索練習 Facebook Graph Api Search 
+ * 
+ * 重點
+ * 1. 利用Graph Api調整出需要的資料
+ * 2. 取得一組Access Token (試著使用 long term token)
+ * 3. 試著用『excel』或任何最簡易的方式，對資料進行探索
+ * 
+ * @author Abola Lee
+ *
+ */
+public class FacebookExample {
+	
+	public static void main(String[] args) {
+		
+		// 遠端資料路徑
+		// [query sample]
+		// search?fields=name,id,likes,talking_about_count&limit=1000&q=靠北&type=page
+		String uri = 
+				"https://graph.facebook.com/v2.5"
+				+ "/search?q=%E5%8F%B0%E5%A4%A7&type=page&limit=100&fields=id,name,likes,talking_about_count"
+				+ "&access_token=EAACEdEose0cBAPszSFpyXdzgmGMhK1uLEnTDbaHSFl1Cabx81lo6saEOOi3NgrJa5mWbYi3QTsMjhUHARLrI6bPXZAWRtdbZBZBOkvz1xgySaKoXqi9UDRYq1cQYoBs5yEu9r2BB4SoGNoZBX0LmvF2vqztPEpqlcTsSa7NssqZAyz4z9x9aIPZB07yv9ZAm4UZD";
+
+
+
+		// Jsoup select 後回傳的是  Elements 物件
+//		[data sample]
+//		----
+//		{
+//			"data": [
+//			{
+//				"name": "靠北工程師",
+//					"id": "1632027893700148",
+//					"likes": 174587,
+//					"talking_about_count": 188119
+//			}
+//		}
+		Elements elems =
+				CrawlerPack.start()
+				.getFromJson(uri)
+				.select("data");
+		
+		String output = "id,名稱,按讚數,討論人數\n";
+		
+		// 遂筆處理
+		for( Element data: elems ){
+			String id = data.select("id").text();
+			String name = data.select("name").text();
+			String likes = data.select("likes").text();
+			String talking_about_count = data.select("talking_about_count").text();
+			
+			output += id+",\""+name+"\","+likes+","+talking_about_count+"\n";
+		}
+		
+		System.out.println( output );
+	} 
+}
